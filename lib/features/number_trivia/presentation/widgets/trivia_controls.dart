0007bed0b0346c3cc715d3136b6d4e150cf0e15b
Oyewale Kehinde <42590579+oyewalekehinde@users.noreply.github.com>
2020-10-28 09:58:26 +0100

    done with project

---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:numberTrivia/features/number_trivia/presentation/bloc/bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _TriviaControlsState createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  String inputStr;
+  final controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller:controller,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(), hintText: "Input a number"),
+          onChanged: (value) {
+            inputStr = value;
+          },
+          onSubmitted: (_){
+             dispatchConcrete();
+          },
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: RaisedButton(
+                child: Text("Search"),
+                color: Theme.of(context).accentColor,
+                textTheme: ButtonTextTheme.primary,
+                onPressed: dispatchConcrete,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: RaisedButton(
+                child: Text("Get random trivia"),
+                onPressed:  dispatchRandom,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete(){
+    controller.clear();
+    BlocProvider.of(context).dispatch(GetTriviaForConcreteNumber(inputStr));
+  }
+   void dispatchRandom(){
+    controller.clear();
+    BlocProvider.of(context).dispatch(GetTriviaForRandomNumber);
+  }
+}
